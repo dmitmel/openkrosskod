a42c1cc81be50f575deba33b0216c00809e42538
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2021-06-14 17:12:15 +0300

    implement Python's FASTCALL convention even though it isn't much faster

--- extras/mega_tournament_ultra_fast.cpp
+++ extras/mega_tournament_ultra_fast.cpp
@@ -2,20 +2,58 @@
 #include <Python.h>
 #include <cstddef>
 #include <cstdint>
+#include <iostream>
 #include <random>
 #include <vector>
 
 namespace mega_tournament_ultra_fast {
 
-PyObject *run_tournament(PyObject *self, PyObject *args) {
+#ifdef METH_FASTCALL
+// FASTCALL doesn't make the function faster in my instance, so it is disabled
+// #define mega_tournament_use_fastcall
+#endif
+
+PyObject *run_tournament(PyObject *self,
+#ifdef mega_tournament_use_fastcall
+                         PyObject *const *args, Py_ssize_t nargs
+#else
+                         PyObject *args
+#endif
+) {
   size_t total_players = 0;
   double champion_win_probability = 0;
   double regular_player_win_probability = 0;
 
+#ifdef mega_tournament_use_fastcall
+
+  const Py_ssize_t required_nargs = 3;
+  if (nargs != required_nargs) {
+    // <https://github.com/python/cpython/blob/v3.9.5/Python/getargs.c#L374-L382>
+    PyErr_Format(PyExc_TypeError,
+                 "function takes exactly %d argument%s (%zd given)",
+                 required_nargs, required_nargs == 1 ? "" : "s", nargs);
+    return nullptr;
+  }
+
+  total_players = PyLong_AsSize_t(args[0]);
+  if (PyErr_Occurred()) {
+    return nullptr;
+  }
+  champion_win_probability = PyFloat_AsDouble(args[1]);
+  if (PyErr_Occurred()) {
+    return nullptr;
+  }
+  regular_player_win_probability = PyFloat_AsDouble(args[2]);
+  if (PyErr_Occurred()) {
+    return nullptr;
+  }
+
+#else
   if (!PyArg_ParseTuple(args, "ldd", &total_players, &champion_win_probability,
                         &regular_player_win_probability)) {
-    return NULL;
+    return nullptr;
   }
+#endif
 
   static thread_local bool rng_initialized = false;
   static thread_local std::default_random_engine rng_engine;
@@ -60,8 +98,18 @@ PyObject *run_tournament(PyObject *self, PyObject *args) {
   return PyLong_FromSsize_t(best_player);
 }
 
+#ifdef mega_tournament_use_fastcall
+_PyCFunctionFast _run_tournament_type_assert = run_tournament;
+#endif
+
 static PyMethodDef py_methods[] = {
-    {"run_tournament", run_tournament, METH_VARARGS, nullptr},
+    {"run_tournament", (PyCFunction)run_tournament,
+#ifdef mega_tournament_use_fastcall
+     METH_FASTCALL,
+#else
+     METH_VARARGS,
+#endif
+     nullptr},
     {nullptr, nullptr, 0, nullptr},
 };
 
